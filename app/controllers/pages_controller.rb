1d89da718a24dbb5c8a05c7a6b453df7d7bd104c
Matthew Deatherage <mdeatherage1992@gmail.com>
2019-08-07 08:46:51 -0400

    descending inbox and outbox

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
 
 def home
   if user_signed_in?
-  @inbox = Message.where(recipient_id: current_user.id)
-  @sent = Message.where(sender_id: current_user.id)
+  @inbox = Message.where(recipient_id: current_user.id).order(created_at: :desc)
+  @sent = Message.where(sender_id: current_user.id).order(created_at: :desc)
   end
 end
 
